0f9bfccb89ea82c6b0e2531ffdeb61da9a879bad
huy <huy@huyng.com>
2013-02-20 02:14:11 -0800

    added redis mem use

---
+++ redis-mem-usage.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""
+Display usage of redis db 
+
+Usage:
+    redis-mem-usage.py [-h HOST]
+
+Options:
+    -h HOST         # host
+   
+"""
+import redis
+from docopt import docopt
+
+def main(host):
+
+    for i in range(15):
+        rd = redis.Redis(host=host, db=i)
+        keys = rd.keys()
+        for k in keys:
+           odbg = rd.debug_object(k)
+           print "size:%s db:%s key:%s" % (odbg.get('serializedLength'), i, k)
+
+    
+if __name__ == '__main__':
+    opts = docopt(__doc__, version="1.0")
+    host = opts['-h']
+    main(host)
+    
+        
+    
+
